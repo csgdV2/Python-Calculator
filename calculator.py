95cd8e4e654346a37ea5a1f2e4c5945f916444ce
csgd <107487586+csgdV2@users.noreply.github.com>
2024-12-03 19:51:09 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Python calculator for addition")
+a = int(input("What is your first number? \n"))
+b = int(input("What is your second number \n"))
+
+print("Your answer is", a+b)
